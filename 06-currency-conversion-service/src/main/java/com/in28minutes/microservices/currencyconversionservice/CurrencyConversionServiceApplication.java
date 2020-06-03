d904649a82abe32c9ea51ca0da4e099c571c054f
Vaibhav Band <vaibhavband@gmail.com>
2020-06-03 17:27:01 +0530

    changes for eureka server

--- 06-currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ 06-currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,12 @@ package com.in28minutes.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.in28minutes.microservices.currencyconversionservice.resource")
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 //@EnableCircuitBreaker
 public class CurrencyConversionServiceApplication {
 
